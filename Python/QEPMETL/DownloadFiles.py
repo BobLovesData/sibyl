eb97ffc4c93fb0a9f1233fe21de51f46a580b483
Bob Wakefield <bob@MassStreet.net>
2022-05-17 23:21:10 -0500

    file sterilization

--- Python/QEPMETL/DownloadFiles.py
+++ Python/QEPMETL/DownloadFiles.py
@@ -4,7 +4,7 @@ import quandl
 import contextlib
 
 
-root_directory = 'C:/FileDepot/'
+root_directory = ''
 yield_curve_data_directory = 'DailyTreasuryYieldCurveData/In/'
 yield_curve_url = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%2011%20and%20year(NEW_DATE)%20eq%202017'
 
@@ -17,9 +17,9 @@ with contextlib.suppress(FileNotFoundError):
 wget.download(yield_curve_url, os.path.join(root_directory, yield_curve_data_directory,'YieldCurveData.xml'))
 
 # Get Quarterly Real Gross Domestic Product
-quarterly_real_gdp = quandl.get("FRED/GDPC1", authtoken="JGJJ9w_o6LJmuKT5SwgM")
+quarterly_real_gdp = quandl.get("FRED/GDPC1", authtoken="")
 quarterly_real_gdp.to_csv(os.path.join(root_directory,quarterly_real_gdp_data_directory,'quarterly_real_gdp.csv'))
 
 # Get Annual Real Gross Domestic Product
-annual_real_gdp = quandl.get("FRED/GDPCA", authtoken="JGJJ9w_o6LJmuKT5SwgM")
+annual_real_gdp = quandl.get("FRED/GDPCA", authtoken="")
 annual_real_gdp.to_csv(os.path.join(root_directory,annual_real_gdp_data_directory,'annual_real_gdp.csv'))
--- Python/QEPMETL/FinancialDataTest.py
+++ Python/QEPMETL/FinancialDataTest.py
@@ -1,7 +1,7 @@
 import quandl
 import pandas
 
-quandl.ApiConfig.api_key = 'ZwazymZ9jtRzzPxqzHwd'
+quandl.ApiConfig.api_key = ''
 test = quandl.get('SF1/PFE_GP_MRQ')
 
 print(test)
